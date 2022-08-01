553359cc9877f51720d805e8a1c638e1cc2de0ab
weiguangfan <826008728@qq.com>
2022-08-01 16:33:20 +0800

    add files

---
+++ array objects/The N-dimensional array(ndarray)/numpy.ndarray/numpy.ndarray.astype.py
@@ -0,0 +1,10 @@
+"""
+ndarray.astype(
+                dtype,
+                order='K',
+                casting='unsafe',
+                subok=True,
+                copy=True)
+数组的拷贝，映射成指定的类型。
+
+"""
