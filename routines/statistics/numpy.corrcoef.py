9cf96f4a6c2a2edec72f52862d78cc5ff7d2992b
weiguangfan <826008728@qq.com>
2022-09-07 15:50:42 +0800

    add files

---
+++ routines/statistics/numpy.corrcoef.py
@@ -0,0 +1,15 @@
+"""
+numpy.corrcoef(
+                x,
+                y=None,
+                rowvar=True,
+                bias=<no value>,
+                ddof=<no value>,
+                *,
+                dtype=None)
+
+返回皮尔逊积距相关系数。
+
+
+"""
+
