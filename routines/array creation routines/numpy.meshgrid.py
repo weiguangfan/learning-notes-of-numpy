707e7b4dabea4b61bc99a5bab489ef6c7df89f9b
weiguangfan <826008728@qq.com>
2022-09-09 11:04:11 +0800

    add files

--- routines/array creation routines/numpy.meshgrid.py
+++ routines/array creation routines/numpy.meshgrid.py
@@ -8,6 +8,11 @@ numpy.meshgrid(
 从坐标向量返回坐标矩阵。
 
 制作N-D坐标阵列，用于在N-D网格上对N-D标量/矢量场进行矢量化评估，给定一维坐标阵列x1, x2,..., xn。
+
+meshgrid对于评估网格上的函数非常有用。
+如果函数依赖于所有坐标，你可以使用参数sparse=True来节省内存和计算时间。
+
+
 Parameters:
 x1, x2,..., x : narray_like
             代表网格坐标的一维数组。
@@ -43,3 +48,40 @@ Returns
     对于x1，第二维，以此类推，xi的元素会沿第一维重复填充矩阵。
 
 """
+import numpy as np
+import matplotlib.pyplot as plt
+# nx,ny = (3,2)
+# x = np.linspace(0,1,nx)
+# print(x)
+# y = np.linspace(0,1,ny)
+# print(y)
+# xv,yv = np.meshgrid(x,y)
+# print(xv)
+# print(yv)
+#
+# xv,yv = np.meshgrid(x,y,sparse=True)
+# print(xv)
+# print(yv)
+
+x = np.linspace(-5,5,101)
+print(x.shape)
+y = np.linspace(-5,5,101)
+print(x.size)
+xx,yy = np.meshgrid(x,y)
+print(xx.shape)
+print(yy.shape)
+
+zz = np.sqrt(xx**2 + yy**2)
+print(zz.shape)
+
+xs,ys = np.meshgrid(x,y,sparse=True)
+zs = np.sqrt(xs**2 + ys**2)
+print(xs.shape)
+print(ys.shape)
+print(zs.shape)
+print(np.array_equal(zz, zs))
+
+h = plt.contourf(x,y,zs)
+plt.axis('scaled')
+plt.colorbar()
+plt.show()
